f0ee6fe1b1fb8d96a5355e77ac97ee5ead78961f
OLianoudakis <gusliaor@student.gu.se>
2020-03-04 14:05:01 +0100

    implemented Orestislevel with new scripts on interactible objects, started on new camera system

---
+++ Assets/CCTVPositionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCamera
+{
+    public class CCTVPositionController : MonoBehaviour
+    {
+        private CameraPositionController m_mainCamera;
+
+        public void ChangeCameraPosition(Vector3 CCTVPosition, CameraCCTVState CCTVStateName)
+        {
+            if (CCTVStateName != m_mainCamera.CameraState)
+            {
+                m_mainCamera.SetPosition(CCTVPosition, CCTVStateName);
+            }
+        }
+
+        private void Start()
+        {
+            m_mainCamera = FindObjectOfType<CameraPositionController>();
+        }
+    }
+}
