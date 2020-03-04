2534182fbc7a07fdaec7a6a095ab20d86a98a63f
Travisko <marek.cern@gmail.com>
2020-03-04 14:17:08 +0100

    Merge.

---
+++ Assets/CCTVPlayerDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameCamera
+{
+    public class CCTVPlayerDetector : MonoBehaviour
+    {
+        [SerializeField]
+        private CameraCCTVState m_CCTVState; //set in inspector
+        [SerializeField]
+        private Transform m_CCTVPosition;
+
+        private CCTVPositionController m_positionController;
+
+        private void Start()
+        {
+            m_positionController = FindObjectOfType<CCTVPositionController>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag.Equals("Player"))
+            {
+                m_positionController.ChangeCameraPosition(m_CCTVPosition.position, m_CCTVState);
+            }
+        }
+    }
+}
