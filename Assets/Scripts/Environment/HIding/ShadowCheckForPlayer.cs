34cca4364f0df6537fc97a3cc687d75982f1cd10
OLianoudakis <gusliaor@student.gu.se>
2020-03-11 11:13:59 +0100

    hiding namespace

---
+++ Assets/Scripts/Environment/HIding/ShadowCheckForPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+namespace Environment.Hiding
+{
+    public class ShadowCheckForPlayer : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag.Equals("Player"))
+            {
+                other.gameObject.GetComponent<PlayerInvisibility>().SetInvisible();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag.Equals("Player"))
+            {
+                other.gameObject.GetComponent<PlayerInvisibility>().SetVisible();
+            }
+        }
+    }
+}
