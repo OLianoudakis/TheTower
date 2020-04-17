faa760e9c7ac44e9ed3183863fb20ae49fe80088
travisko <marek.cern@gmail.com>
2020-04-17 16:01:50 +0200

    Player changes layer to Shadows layer when in shadows to not be seen by light.

--- Assets/Scripts/Player/PlayerInvisibility.cs
+++ Assets/Scripts/Player/PlayerInvisibility.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Player.EmptyClass;
 
 namespace Player
 {
@@ -9,13 +10,17 @@ namespace Player
     {
         [SerializeField]
         private CanvasGroup m_invisibilityCanvas;
+
         [SerializeField]
         private float m_fadeLerpSpeed = 1.0f;
-        private float m_fadeLerpInterval = 0.0f;
-        private bool m_currentlyFading = false;
-        private int m_invisibleCount = 0;
-        private bool m_isInvisible;
 
+        private List<GameObject> m_playerGameObjects;
+        private float m_fadeLerpInterval = 0.0f;
+        private int m_invisibleCount = 0;
+        private int m_defaultLayer;
+        private bool m_currentlyFading = false;
+        private bool m_isInvisible;
+        
         public bool isInvisible
         {
             get { return (m_invisibleCount > 0); }
@@ -23,13 +28,15 @@ namespace Player
 
         public void SetInvisible()
         {
-            
             if (++m_invisibleCount == 1)
             {
                 m_isInvisible = true;
                 m_currentlyFading = true;
+                foreach (GameObject playerGameObject in m_playerGameObjects)
+                {
+                    playerGameObject.layer = LayerMask.NameToLayer("Shadows");
+                }
             }
-            //StartCoroutine(ShadowUIFade(true));
         }
 
         public void SetVisible()
@@ -38,13 +45,30 @@ namespace Player
             {
                 m_isInvisible = false;
                 m_currentlyFading = true;
+                foreach (GameObject playerGameObject in m_playerGameObjects)
+                {
+                    playerGameObject.layer = m_defaultLayer;
+                }
             }
-            //StartCoroutine(ShadowUIFade(false));
+        }
+
+        private void Awake()
+        {
+            GameObject[] gobjects = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+            m_playerGameObjects = new List<GameObject>();
+            for (int i = 0; i < gobjects.Length; i++)
+            {
+                if (gobjects[i].layer == LayerMask.NameToLayer("Player"))
+                {
+                    m_playerGameObjects.Add(gobjects[i]);
+                }
+            }
         }
 
         private void Start()
         {
             m_isInvisible = false;
+            m_defaultLayer = gameObject.layer;
         }
 
         private void Update()
@@ -75,42 +99,5 @@ namespace Player
                 }
             }
         }
-
-        //private IEnumerator ShadowUIFade(bool fadeIn)
-        //{
-        //    bool currentlyFading = true;
-        //    if (fadeIn)
-        //    {
-        //        while (currentlyFading)
-        //        {
-        //            m_invisibilityCanvas.alpha = Mathf.Lerp(0.0f, 1.0f, m_fadeLerpInterval);
-        //            m_fadeLerpInterval += m_fadeLerpSpeed * Time.deltaTime;
-        //            yield return new WaitForEndOfFrame();
-        //            if (m_fadeLerpInterval > 1.0f)
-        //            {
-        //                m_invisibilityCanvas.alpha = 1.0f;
-        //                m_fadeLerpInterval = 0.0f;
-        //                currentlyFading = false;
-        //                yield return null;
-        //            }
-        //        }
-        //    }
-        //    else
-        //    {
-        //        while (currentlyFading)
-        //        {
-        //            m_invisibilityCanvas.alpha = Mathf.Lerp(1.0f, 0.0f, m_fadeLerpInterval);
-        //            m_fadeLerpInterval += m_fadeLerpSpeed * Time.deltaTime;
-        //            yield return new WaitForEndOfFrame();
-        //            if (m_fadeLerpInterval > 1.0f)
-        //            {
-        //                m_invisibilityCanvas.alpha = 0.0f;
-        //                m_fadeLerpInterval = 0.0f;
-        //                currentlyFading = false;
-        //                yield return null;
-        //            }
-        //        }
-        //    }
-        //}
     }
 }
